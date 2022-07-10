426cdb88b3f4db00c9fcd581190dc2341c5acf4b
Stanislav Stanchev <stanislav.stanchev@netea.bg>
2022-07-10 12:14:56 +0300

    Create Not Found Page and add style for page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,39 @@
+import { useNavigate } from 'react-router-dom';
+import { Typography, Button, Card, CardContent, CardMedia, CardActions } from '@mui/material';
+
+import errorPageImage from '../images/errorPageImage.jpg';
+
 export default function NotFoundPage() {
-    return(
-        <div>
-            <p>Not Found</p>
-        </div>
+    const navigate = useNavigate();
+
+    function goToHomePage() {
+        navigate('/')
+    }
+
+    return (
+        <Card sx={{ maxWidth: 500, margin: 'auto', width: '50%', marginTop: '10%', boxShadow: '0px 0px 15px 7px #9B9B9B' }}>
+            <CardMedia
+                component="img"
+                alt="Error Image"
+                image={errorPageImage}
+            />
+            <CardContent sx={{ textAlign: 'center' }}>
+                <Typography gutterBottom variant="h3" component="div">
+                    404
+                </Typography>
+                <Typography variant="h3" color="text.secondary" sx={{ fontSize: '25px' }}>
+                    Something went WRONG!
+                </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'center' }}>
+                <Button
+                    sx={{ color: 'white' }}
+                    className='marker-drawer-time-btn'
+                    size="small"
+                    onClick={goToHomePage}
+                >
+                    Back to home page</Button>
+            </CardActions>
+        </Card>
     )
 }
